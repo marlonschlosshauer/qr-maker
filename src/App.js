02889f5fcfe96bd7a341d9a36e1f3f7f6441b784
marlonschlosshauer <marlonschlosshauer@gmail.com>
2021-11-17 15:19:25 +0100

    show qr and make editable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import QRCode from "qrcode.react";
+import { useState } from "react";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	const [text, setText] = useState();
+	return (
+		<div className="App">
+			<div className="input-container">
+				<textarea name="qr" rows="5" cols="50" type="textarea" placeholder="Enter QR text" value={text} onChange={(e) => setText(e.target.value)} />
+			</div>
+			<div className="qr-container">
+				<QRCode className='qr-code' value={text} renderAs='png' size={300} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
