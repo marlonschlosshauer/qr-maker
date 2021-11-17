94a8f06d8a386648a7225d17583c6fcfa8f4a681
marlonschlosshauer <marlonschlosshauer@gmail.com>
2021-11-17 15:42:22 +0100

    title

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import QRCode from "qrcode.react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import './App.css';
 
 const App = () => {
 	const [text, setText] = useState('');
+
+	useEffect(() => document.title = 'QR-maker', [])
+
 	return (
 		<div className="App">
 			<div className="input-container">
-				<textarea name="qr" rows="5" cols="50" type="textarea" placeholder="Enter QR text" value={text} onChange={(e) => setText(e.target.value)} />
+				<textarea name="qr" rows="5" cols="50" type="textarea" placeholder="Enter QR text" value={text} onChange={({ target }) => {
+					document.title = target.value;
+					setText(target.value)
+				}} />
 			</div>
 			<div className="qr-container">
 				<QRCode className='qr-code' value={text} renderAs='png' size={300} />
