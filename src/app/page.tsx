341e8e38138a85479945c143046138117d9da791
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2023-11-09 17:37:02 +0100

    Quick rebranding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export async function generateMetadata({ searchParams }: any) {
   const { q } = searchParams;
   return {
     metadataBase: new URL(process.env.BASE_URL ?? ""),
-    title: q ? `URL: ${q}` : "Embed: QR Code Generator",
+    title: q ? `URL: ${q}` : "Embed-One: QR Code Generator",
     description: "Easily generate, preview and download QR codes from urls.",
     keywords: ["QR", "code", "generation"],
     openGraph: {
